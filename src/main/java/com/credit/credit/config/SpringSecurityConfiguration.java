5aa31b8a5023e89b5bb65b6d275be60408156e20
sezakocadere <seza.kocadere@gmail.com>
2023-02-24 14:38:32 +0300

    add jwt authentication config

---
+++ src/main/java/com/credit/credit/config/SpringSecurityConfiguration.java
@@ -0,0 +1,54 @@
+package com.credit.credit.config;
+
+import com.credit.credit.util.JwtTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    // define permissions on endpoints
+    private static final String[] AUTH_WHITELIST = {
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            "/v2/api-docs/**",
+            "/swagger-ui/**",
+            "/customers",
+            "/customers/**",
+            "/loan/**",
+            "/loan",
+            "/h2-console/**",
+            "/h2-console"
+    };
+    @Autowired
+    private JwtTokenFilter jwtTokenFilter;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // configure for an authentication manager with the correct password-encoding schema that will be used for credentials verification
+        return new BCryptPasswordEncoder(); // use the bcrypt password-hashing algorithm
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // configure for web security
+        http.csrf().disable()
+                .authorizeRequests().antMatchers(AUTH_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // set session management to stateless
+        http.headers().frameOptions().disable();
+        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
---
+++ src/main/java/com/credit/credit/util/JwtTokenFilter.java
@@ -0,0 +1,45 @@
+package com.credit.credit.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String username = null;
+        String token = null;
+        if (Objects.nonNull(header) && header.startsWith("Bearer ")) {
+            token = header.split(" ")[1].trim();
+            username = JwtTokenUtil.extractUsername(token);
+        }
+        if (Objects.nonNull(username) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+            if (JwtTokenUtil.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        }
+        chain.doFilter(request, response);
+    }
+}
